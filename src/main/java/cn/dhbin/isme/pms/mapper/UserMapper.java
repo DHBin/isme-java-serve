ad9caa2c21219aa84e1bf1e2fb5eea9a45d52fad
dhb <xx158@qq.com>
2024-01-05 08:59:10 +0800

    fix: 用户性别过滤无效果

--- src/main/java/cn/dhbin/isme/pms/mapper/UserMapper.java
+++ src/main/java/cn/dhbin/isme/pms/mapper/UserMapper.java
@@ -1,7 +1,10 @@
 package cn.dhbin.isme.pms.mapper;
 
+import cn.dhbin.isme.pms.domain.dto.UserPageDto;
 import cn.dhbin.isme.pms.domain.entity.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * UserMapper
@@ -9,5 +12,18 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author dhb
  */
 public interface UserMapper extends BaseMapper<User> {
+
+    /**
+     * 分页查询
+     *
+     * @param page     分页
+     * @param username 用户名
+     * @param gender   性别
+     * @param enable   状态
+     * @return 分页结果
+     */
+    IPage<UserPageDto> pageDetail(IPage<User> page, @Param("username") String username, @Param("gender") Integer gender,
+                                  @Param("enable")
+                                  Boolean enable);
 
 }
